e3b33a0c0fc3f7bec6dc138f406f7581ab293d2b
Hugo <hugo.vovard@wanadoo.fr>
2023-06-30 09:20:24 +0200

    Added the possibility to restore data.

--- backup.sh
+++ backup.sh
@@ -4,40 +4,40 @@
 
 # Color variables
 red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
 reset=$(tput sgr0)
 
 # Get information from template file
 function get_config () {
-directories=$(cat config_test.json | jq ".DIRECTORIES[]" --raw-output)
-backup_path=$(cat config_test.json | jq ".BACKUP_PATH" --raw-output)
+	directories=$(cat config_test.json | jq ".DIRECTORIES[]" --raw-output)
+	backup_path=$(cat config_test.json | jq ".BACKUP_PATH" --raw-output)
 }
 
-# Try to save file in $1 in the backup_path
-function save_user_files() {
-	local file=$1
-	printf "[+] Saving "${file}" ...
-"
-	if [ -d ""${file}"" ]
+# Try to save src ($1) in dst ($2)
+function sync_user_files() {
+	local src="${1}"
+	local dst="${2}"
+	printf "[+] Saving "${src}" in "${dst}" ...\n"
+	if [ -d ""${src}"" ]
     then
-       	local output=$(printf ""${file}"" | awk -F "/" 'NF{OFS="/";NF-=1};1')
-       	rsync -avh --mkpath "${file}" "${backup_path}""${output}" --delete
+       	rsync -avh --mkpath "${src}"/ "${dst}" --delete
     else
-       	rsync -avh --mkpath "${file}" "${backup_path}""${file}" --delete
+       	rsync -avh --mkpath "${src}" "${dst}" --delete
     fi
 }
 
-# Try to save file in $1 in the backup_path throught sudo
-function save_privileged_files() {
-	local file=$1
-	printf "!!! "${file}" could not be save, retrying with sudo !!!"
-	printf "[+] Saving "${file}" ...
-"
- 	if [ -d ""${file}"" ]
-   	then
-    	local output=$(printf ""${file}"" | awk -F "/" 'NF{OFS="/";NF-=1};1')
-        sudo rsync -avh --mkpath "${file}" "${backup_path}""${output}" --delete
+# Try to save src ($1) in dst ($2) throught sudo
+function sync_privileged_files() {
+	local src="${1}"
+	local dst="${2}"
+	printf "${yellow}!!! "${src}" could not be save, retrying with sudo !!!\n${reset}"
+	printf "[+] Saving "${src}" in "${dst}" ...\n"
+	if [ -d ""${src}"" ]
+    then
+       	sudo rsync -avh --mkpath "${src}"/ "${dst}" --delete
     else
-       	sudo rsync -avh --mkpath "${file}" "${backup_path}""${file}" --delete
+       	sudo rsync -avh --mkpath "${src}" "${dst}" --delete
     fi
     
 }
@@ -49,17 +49,32 @@ function error() {
 
 function main(){
 	get_config
+	local flag=$1
 	for file in ${directories}
 	do
-		save_user_files "${file}"
+		# Check backup flag (either backup or restore)
+		if [[ "${flag}" == "-b" ]]
+		then
+			local src="${file}"
+			local dst="${backup_path}""${file}"
+		elif [[ "${flag}" == "-r" ]]
+		then
+			local src="${backup_path}""${file}"
+			local dst="${file}"
+		else
+			exit
+		fi
+
+		# Try to sync as current user. If failed, try again as sudo.
+		sync_user_files "${src}" "${dst}"
 		if [ $? -eq 0 ]
 		then
-		   	printf "[+][+] "${file}" saved !\n"
+		   	printf "${green}[+][+] "${file}" saved !\n${reset}"
 		else
-			save_privileged_files "${file}"
+			sync_privileged_files "${src}" "${dst}"
 			if [ $? -eq 0 ]
 			then
-				printf "[+][+] "${file}" saved !\n"
+				printf "${green}[+][+] "${file}" saved !\n${reset}"
 			else
 				error "exit $? "${file}" could not be saved"
 				exit 1
@@ -69,4 +84,4 @@ function main(){
 	exit 0
 }
 
-main
+main $*
